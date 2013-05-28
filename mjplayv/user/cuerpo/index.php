3b48791a56bd5d7974a380ab59ae4c90141d7df7
jorge <jorgedipra@gmail.com>
2013-05-28 00:25:37 -0500

    login

--- mjplayv/user/cuerpo/index.php
+++ mjplayv/user/cuerpo/index.php
@@ -1,21 +1,42 @@
+      <div id="outer-circle">
+        <div id="inner-circle">
+          <div id="center-circle">
+            <div id="content">
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1>log</h1>
 <section id="tabs">
-
+  <nav>
    <ul>
-    <li><a href= "#tabs-1" >Iniciar</a></li>
-    <li><a href= "#tabs-2" >Registrarse</a></li>
+    <li id="INI"><a href= "#tabs-1" >Iniciar</a></li>
+    <li id="REG"><a href= "#tabs-2" >Registrarse</a></li>
   </ul>   
-
+  </nav>
  <div class="target">
   <div class="glass"></div>  
     <article  class="body" >
     <div id="tabs-1">
-         <FORM action="index.php" method="post">  
-                    Apodo: 
-                         <INPUT name="Apodo" type="text" id="firstname" maxlength="7" ><BR>
-                    <INPUT type="submit" value="enviar" onclick="">
-                    <input type="hidden" name="ES" value="On" />
-                    <INPUT type="reset">       
-                 </form> 
+      <h2><- Login  In -></h2>
+        <section id="UsuCon">
+           <form action="index.php" method="post">             
+              <div class="clearfix">
+                <label>Username:</label>
+                <input name="Apodo" type="text" id="firstname" maxlength="7" placeholder="Username" >
+              </div>
+              <div class="clearfix">
+                <label>Password:</label>
+                <input type="password" placeholder="Password">
+              </div>
+              <button class="btn" type="submit">Sign in</button>
+              <input type="hidden" name="ES" value="On" />              
+               </form> 
+        </section>
+        <section id="div-border"></section>
+        <section id="UsuFaTwG">
+         
+        </section>
      </div>   
   <div id="tabs-2">
     registar 
@@ -24,3 +45,13 @@
   </div>
  </div>
 </section>
+
+
+</head>
+<body>
+ 
+     
+           
+         
+
+
