b24079b7923f9c2128f7a5030ee8d8a36f98c284
MaterBit <julecci@gmail.com>
2013-06-30 18:40:27 -0500

    avances del agregar noticias

---
+++ mjplayv/Noticias/agregarnoticias.php
@@ -0,0 +1,49 @@
+<?php
+ session_start();
+ include_once ('../recursos/info.php');//se llama la informacion de la pagina
+?>
+<link rel="stylesheet" href="../css/style.css"><!--se llama el stilo-->
+<link rel="stylesheet" href="../css/stylehome.css">
+<link rel="stylesheet" href="css/sytileNoticias.css">
+<script src="../js/jquery-1.9.1.min.js"></script>
+<script src="../js/jquery-ui.js"></script>
+<script src="../script/scriptp.js"></script>
+<script src="../script/scriptpins.js"></script>
+</head>
+<body>
+ <?php
+   include "../conexion/storage/conexion.php"; //incluimos el archivo de conexion de storage
+   include "../conexion/php/operacionesSql.php";
+   include "script/script.php";
+    $view= new stdClass(); 
+    $view->disableLayout=false;
+     /**
+     * [$view->objeto de validación]
+     * @var boolean
+     */
+  $destinoF="";
+     if(isset($_POST['artNombre']))
+       {
+
+         if($_FILES['artimagen']['tmp_name']!=""){
+    $prefijo = substr(md5(uniqid(rand())),0,6);
+    $destino =  "../../img/user/".$prefijo."_".$_FILES['artimagen']['name'];
+    $destinoF = '/img/noticias/'.$prefijo."_".$_FILES['artimagen']['name'];
+    copy($_FILES['artimagen']['tmp_name'],$destino);  }
+
+        $maxid ="SELECT MAX(ArtId) FROM articulo";
+         $query="INSERT INTO articulo(ArtNombre,ArtContenido,ArtFecha,ArtReferencia,ArtImagen,sintexis)
+         VALUES('".$_POST['artNombre']."','".$_POST['artreacontenido']."','".$_POST['artNombre']."','".$_POST['artreferencias']."','".$destinoF."','".$_POST['sintexis']."')";
+
+         $query2="INSERT INTO ctgarg(ArtId,CtgId)VALUES();";
+         $query3="INSERT INTO artcolaborador(ArtId,UsrId,colaboradorPermisos)VALUES('','".$_SESSION['Wusuario']."','');"
+       }
+     $objopera = new  operaciones();
+     $resultado = $objopera->buscar($consulta);
+    if ($view->disableLayout==false)
+    { 
+      include_once ('cuerpo/agregar.php');//se llama el cuerpo
+    }    
+?>
+</body>
+</html>
---
+++ mjplayv/Noticias/cuerpo/agregar.php
@@ -0,0 +1,11 @@
+
+<form action="agregarnoticias.php" method="post" enctype="multipart/form-data">
+		nombre del articulo <input type="text" name="artNombre" value="">
+		sintexis <textarea name="artsintexis" value=""> </textarea>
+		resumen <textarea name="artresumen" value=""></textarea>
+		contenido <textarea name="artreacontenido" value=""></textarea>
+		imagenalusiva <input type="file" name="artimagen" value=""/>
+		referencias <input name="artreferencias" value="">
+		<button name="insertar">Insertar</button>
+</form>
+
